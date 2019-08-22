19f6b21bfd7b2126cd2fec759bac3259fe1dd921
onyekachila <onyekachila@gmail.com>
2019-08-22 01:40:27 +0100

    Tested Out DI
    Tested Dependency Injection using the IMessageService interface

---
+++ aspnetcorehousekeeping/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using aspnetcorehousekeeping.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace aspnetcorehousekeeping
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(provider => Configuration); 
+            services.AddSingleton<IMessageService, HardcodedMessageService>();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json");
+
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IMessageService msg)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                //await context.Response.WriteAsync("Hello World!");
+                //var message = Configuration["Message"];
+                await context.Response.WriteAsync(msg.GetMessage());
+            });
+        }
+    }
+}
